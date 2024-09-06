52e604c9cff29e31025a7ccb466c77b7b0c9645a
Yang Wang <wyatuestc@gmail.com>
2024-09-06 04:15:51 -0700

    update run

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export NUMEXPR_MAX_THREADS=96
-for seed in {0..3}
+for seed in {4..7}
 do
-  CUDA_VISIBLE_DEVICES=7 python -m quantize_llama.hessian_offline_llama \
+  CUDA_VISIBLE_DEVICES=6 python -m quantize_llama.hessian_offline_llama \
     --base_model meta-llama/Meta-Llama-3.1-405B-Instruct \
     --save_path Hessians-Llama-31-405B-Instruct-6144-8k-seed-${seed} \
     --batch_size 1 --devset_size 1536 --ctx_size 8192 \
